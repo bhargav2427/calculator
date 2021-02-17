adb4da9442591163cb0089aabdf40397b67789b3
bhargav2427 <akhani.bharga@gmail.com>
2021-02-17 14:19:04 +0530

    Logic done

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
-function abc(x) {
-  console.log(x.innerHTML);
+var expression = "";
+var b = document.getElementsByTagName("button");
+
+//b.splice(b.length - 1, b.length);
+
+for (var i = 0; i < b.length - 1; i++) {
+  b[i].addEventListener("click", function () {
+    showLog(this.innerHTML);
+  });
+}
+document.getElementById("equals").addEventListener("click", function () {
+  // console.log(expression);
+  equals();
+});
+
+function equals() {
+  console.log(eval(expression));
+  // console.log("pressed");
 }
 
-document.getElementById("nn").addEventListener("click", abc(this));
+function showLog(x) {
+  expression += x;
+  console.log(expression);
+}
